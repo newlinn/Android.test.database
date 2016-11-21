c29559d5f9187327ffdd32688eabbf2ceff19179
newlinn <newlinn@qq.com>
2016-11-22 00:03:04 +0800

    Upgrade db

--- app/src/main/java/com/project/androidtestdatabase/MyDatabaseHelper.java
+++ app/src/main/java/com/project/androidtestdatabase/MyDatabaseHelper.java
@@ -12,18 +12,17 @@ import android.widget.Toast;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
 
-
     Context mContext;
 
     public MyDatabaseHelper(Context context, String name,
-                            SQLiteDatabase.CursorFactory cursorFactory, int version){
+                            SQLiteDatabase.CursorFactory cursorFactory, int version) {
         super(context, name, cursorFactory, version);
         this.mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-         final String CREATE_BOOK = "CREATE TABLE book ("
+        final String CREATE_BOOK = "CREATE TABLE book ("
                 + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + " author TEXT,"
                 + " price REAL,"
@@ -36,15 +35,27 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        if (i==1 && i1 == 2) {
-            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS category");
-            final String CREATE_CATEGORY = "CREATE TABLE category ("
-                    + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + " category_name TEXT,"
-                    + " category_code INTEGER"
-                    + ")";
-            sqLiteDatabase.execSQL(CREATE_CATEGORY);
-            Toast.makeText(this.mContext, "CREATE TABLE category", Toast.LENGTH_SHORT).show();
+        switch (i) {
+            case 1: {
+                sqLiteDatabase.execSQL("DROP TABLE IF EXISTS category");
+                final String CREATE_CATEGORY = "CREATE TABLE category ("
+                        + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                        + " category_name TEXT,"
+                        + " category_code INTEGER"
+                        + ")";
+                sqLiteDatabase.execSQL(CREATE_CATEGORY);
+                Toast.makeText(this.mContext, "CREATE TABLE category", Toast.LENGTH_SHORT).show();
+            }
+            ;
+            case 2: {
+                final String add_book_category = "ALERT TABLE book"
+                        + " COLUMN category_id INTEGER";
+                sqLiteDatabase.execSQL(add_book_category);
+                Toast.makeText(this.mContext, "ALTER TABLE book", Toast.LENGTH_SHORT).show();
+            }
+            default: {
+
+            }
         }
     }
 
