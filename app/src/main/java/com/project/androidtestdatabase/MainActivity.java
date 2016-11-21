76cec7f7b736d19267795552c14e99ca7fe66832
℃ <newlinn@qq.com>
2016-11-21 22:21:49 +0800

    Add / Update

--- app/src/main/java/com/project/androidtestdatabase/MainActivity.java
+++ app/src/main/java/com/project/androidtestdatabase/MainActivity.java
@@ -1,13 +1,62 @@
 package com.project.androidtestdatabase;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO: 2016/11/20 database version
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+
+        Button btn_create_datebase = (Button)findViewById(R.id.btn_create_datebase);
+        btn_create_datebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+
+            }
+        });
+
+        Button btn_add_data = (Button)findViewById(R.id.btn_add_data);
+        btn_add_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The DDa Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("book", null, values);
+                values.clear();
+
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("book", null, values);
+            }
+        });
+
+        Button btn_update_data = (Button)findViewById(R.id.btn_update_data);
+        btn_update_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 15.99);
+                db.update("book", values, "name = ?", new String[]{"The Lost Symbol"});
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/project/androidtestdatabase/MyDatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.project.androidtestdatabase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by newli on 2016/11/20.
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+
+
+    Context mContext;
+
+    public MyDatabaseHelper(Context context, String name,
+                            SQLiteDatabase.CursorFactory cursorFactory, int version){
+        super(context, name, cursorFactory, version);
+        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+         final String CREATE_BOOK = "CREATE TABLE book ("
+                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + " author TEXT,"
+                + " price REAL,"
+                + " page INTEGER,"
+                + " name TEXT"
+                + ")";
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        Toast.makeText(this.mContext, "CREATE TABLE book", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        if (i==1 && i1 == 2) {
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS category");
+            final String CREATE_CATEGORY = "CREATE TABLE category ("
+                    + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + " category_name TEXT,"
+                    + " category_code INTEGER"
+                    + ")";
+            sqLiteDatabase.execSQL(CREATE_CATEGORY);
+            Toast.makeText(this.mContext, "CREATE TABLE category", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
